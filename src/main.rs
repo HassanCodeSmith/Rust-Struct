6df460406fe4bb1be9b7ff65295eb71f35260b19
Hassan Murtaza <hassan.murtaza@smartfunstudios.com>
2024-08-21 18:59:36 +0500

    struct tuples and unit like structs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
-/** ------------------------------------------------
- *  Defining and Instantiating Structs
- *  Using the Field Init Shorthand
- *  Creating Instances from Other Instances with Struct Update Syntax
- ** ------------------------------------------------*/
+/*
+----------------------------------------------------------------------------
+Defining and Instantiating Structs
+Using the Field Init Shorthand
+Creating Instances from Other Instances with Struct Update Syntax
+----------------------------------------------------------------------------
+*/
 
+/*
 #[derive(Debug)]
 #[allow(dead_code)]
 struct User {
@@ -36,4 +39,58 @@ fn main() {
     };
 
     println!("Personal Credentials: {:#?}", user2);
+}
+*/
+
+/*
+----------------------------------------------------------------------------
+    Using Tuple Structs Without Named Fields to Create Different Types
+----------------------------------------------------------------------------
+*/
+/*
+#[allow(dead_code)]
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+fn main() {
+    #[allow(unused_variables)]
+    let black: Color = Color(34, 345, 23);
+    let origin: Point = Point(23, 45, 54);
+    let Point(twenty_three, fourty_five, fifty_four) = origin;
+
+    println!("Sturct Tuple (Destructure):");
+    println!("Twenty Three: {twenty_three}");
+    println!("Fourty Five: {fourty_five}");
+    println!("Fifty Four: {fifty_four}");
+
+    println!("\nSturct Tuple (Dot Notation):");
+    println!("Black index 1: {:?}", black.0);
+    println!("Black index 2: {:?}", black.1);
+    println!("Black index 3: {:?}", black.2);
+
+    let tup = ("Hassan", 25, 43000);
+    let (name, age, salary) = tup;
+
+    println!("\nSimple Tuple (Destructuring):");
+    println!("Name: {name}");
+    println!("Age: {age}");
+    println!("Salary: {salary}");
+
+    println!("\nSimple Tuple (Dot Notation):");
+    println!("Name: {:?}", tup.0);
+    println!("Age: {:?}", tup.1);
+    println!("Salary: {:?}", tup.2);
+}
+
+*/
+
+/*
+----------------------------------------------------------------------------
+    Unit-Like Structs Without Any Fields
+----------------------------------------------------------------------------
+*/
+struct AlwaysEqual;
+
+fn main() {
+    #[allow(unused_variables)]
+    let subject = AlwaysEqual;
 }
